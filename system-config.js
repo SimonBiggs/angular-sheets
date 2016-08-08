074dbc2c3f8ae6cbc67c90e3d09ed21e11d373a6
SimonBiggs <mail@simonbiggs.net>
2016-08-09 00:46:30 +1000

    began http

--- system-config.js
+++ system-config.js
@@ -8,6 +8,7 @@
 /** Map relative paths to URLs. */
 var map = {
     '@angular2-material': 'vendor/@angular2-material',
+    'angular2-in-memory-web-api': 'vendor/angular2-in-memory-web-api'
 };
 /** User packages configuration. */
 var packages = {
@@ -15,7 +16,8 @@ var packages = {
     '@angular2-material/button': { main: 'button.js' },
     '@angular2-material/sidenav': { main: 'sidenav.js' },
     '@angular2-material/toolbar': { main: 'toolbar.js' },
-    '@angular2-material/list': { main: 'list.js' }
+    '@angular2-material/list': { main: 'list.js' },
+    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' }
 };
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
